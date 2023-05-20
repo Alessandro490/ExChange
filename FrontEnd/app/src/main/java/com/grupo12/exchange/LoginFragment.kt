b76fc8285e935abc50e8dd849bb5c63fb5145970
Alessandro490 <00101519@example.com>
2023-05-20 09:59:23 -0600

    Navbar implementation

--- FrontEnd/app/src/main/java/com/grupo12/exchange/LoginFragment.kt
+++ FrontEnd/app/src/main/java/com/grupo12/exchange/LoginFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class LoginFragment : Fragment() {
+
+    private lateinit var actionToSignup: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,4 +23,15 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        actionToSignup = view.findViewById(R.id.sign_up_button)
+
+        actionToSignup.setOnClickListener{
+            it.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+    }
+
 }
